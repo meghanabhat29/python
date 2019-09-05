44ff14cfeb091ca453b26dc4a602789f8ad2af4c
Meghana Bhat <42279550+meghanabhat29@users.noreply.github.com>
2019-09-06 01:06:24 +0530

    Create a2_10.py

---
+++ a2_10.py
@@ -0,0 +1,9 @@
+students={
+    '118':{'Name':'Kanha','Age':20,'School':'Aquamarine'},
+    '147':{'Name':'Meghana','Age':20,'School':'Hogwarts'},
+    '152':{'Name':'Yashu','Age':17,'School':'Maze'},
+    '156':{'Name':'Hermione','Age':20,'School':'Durmstrang'}
+    }
+for a in students:
+    if a%2==0:
+        print(a," :",students[a])

caacde82b865e014c755df5190a5ae12aabdb7e3
Meghana Bhat <42279550+meghanabhat29@users.noreply.github.com>
2019-09-06 01:12:18 +0530

    Create a2_11.py

---
+++ a2_11.py
@@ -0,0 +1,6 @@
+AtomicDictionary={1:'H',2:'He',3:'Li',4:'Be',6:'C'}
+AtomicDictionary.append(input("Enter an item : "))
+AtomicDictionary.append(input("Enter an item : "))
+print(Atomicdictionary)
+print("The number of elements in this dictionary are : ",len(AtomicDictionary))
+        
